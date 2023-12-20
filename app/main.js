b57664ba3d22dad537e77ac9a37df6b6fd1afbf0
Frontend_Oleh_K <oleh.kotlinskyi@abz.agency>
2023-12-20 18:53:48 +0200

    pass 4 stage

--- app/main.js
+++ app/main.js
@@ -1,8 +1,18 @@
 const net = require('net');
 
 const server = net.createServer((socket) => {
-  socket.on('data', () => {
-    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+  socket.on('data', (data) => {
+    const requestData = data.toString();
+
+    const [requestLine, ...headers] = requestData.split('\r\n');
+
+    const [_method, path, _httpVersion] = requestLine.split(' ');
+
+    if (path === '/') {
+      socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    } else {
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+    }
   });
 
   socket.on('close', () => {
