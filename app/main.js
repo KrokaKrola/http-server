abc6bf86438711aa10be406ecfaad54209f58fd3
Frontend_Oleh_K <oleh.kotlinskyi@abz.agency>
2023-12-20 18:46:54 +0200

    pass 2 stage

--- app/main.js
+++ app/main.js
@@ -1,7 +1,12 @@
 const net = require('net');
 
 const server = net.createServer((socket) => {
+  socket.on('data', () => {
+    socket.write('HTTP/1.1 200 OK\r\n');
+  });
+
   socket.on('close', () => {
+    console.log('client disconnected');
     socket.end();
     server.close();
   });
