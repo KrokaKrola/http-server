21f261978d19b16ec0a0750953c74ba2fd714bed
Frontend_Oleh_K <oleh.kotlinskyi@abz.agency>
2023-12-20 18:48:45 +0200

    pass 3 stage

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ const net = require('net');
 
 const server = net.createServer((socket) => {
   socket.on('data', () => {
-    socket.write('HTTP/1.1 200 OK\r\n');
+    socket.write('HTTP/1.1 200 OK\r\n\r\n');
   });
 
   socket.on('close', () => {
